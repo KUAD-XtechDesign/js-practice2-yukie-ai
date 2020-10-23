7aed0393a101ca1a1ca1693207e5507402ecabb3
yukie-ai <68629886+yukie-ai@users.noreply.github.com>
2020-10-23 15:38:08 +0900

    更新

--- sample/js/script01.js
+++ sample/js/script01.js
@@ -1,5 +1,35 @@
 $(function(){
-
+    let txt1 = "おはようございます！";
+    let txt2 = "おげんきですか？";
+    let num1 = 20;
+    let num2 = 32;
+    　　
+    $("#button01").on("click",function(){
+        $("#content").text("こんにちは！");
+    })
+    
+    $("#button02").on("click",function(){
+        $("#content").text(txt1);
+    })
+    
+    $("#button03").on("click",function(){
+        $("#content").text(txt1 + txt2);
+    })
+    
+    $("#button04").on("click",function(){
+        $("#content").text(num1 + num2);
+    })
+    
+    // + - * /
+    
+    $("#button05").on("click",function(){
+        num1 = num1 + 10;
+      //$("#content").text(num1);
+    
+        $("#ball").css("margin-left",num1+'px')
+    })
+    
+    
 
 
 
--- sample/js/script02.js
+++ sample/js/script02.js
@@ -1,5 +1,17 @@
 $(function(){
 
+    let hako=0;
+
+    function checkBox(){
+        if(hako>5){
+            $("#content").text(hako+"個入っています。箱はいっぱいです。箱を空にします。");
+            hako=0;
+        }else{ 
+            $("#content").text(hako+"個入っています。まだ入ります。");
+        }
+    }
+
+    $
 
 
 })
