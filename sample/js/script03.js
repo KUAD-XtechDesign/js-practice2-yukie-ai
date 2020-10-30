a37145def880a6b57df9640b2fe05bba9e44e98a
yukie-ai <68629886+yukie-ai@users.noreply.github.com>
2020-10-30 15:33:59 +0900

    Update script03.js

--- sample/js/script03.js
+++ sample/js/script03.js
@@ -1,5 +1,32 @@
 $(function(){
+    
+    let hako =  0;
+
+    $("#button01").on("click",function(){
+        for(let i=0; i < 10; i++){
+            hako = hako + 2;
+        }
+        $("#content").text(hako);
+    })
+
+    $("#button02").on("click",function(){
+        for(let i=0; i < 10; i++){
+            hako = hako + i;
+        }
+        $("#content").text(hako);
+    })
+
+    $("#button03").on("click",function(){
+        setInterval(function(){
+            hako = hako + 1;
+            $("#content").text(hako);
+        },1000)
+    })
+
+    $("#button04").on("click",function(){
+        hako = 0;
+        $("#content").text(hako);
+    })
 
 
-  
 })
